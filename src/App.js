40404ca453ef0b1546447ab970f20ab9e8d3b711
domonikk <45376672+domonikk@users.noreply.github.com>
2021-04-09 21:18:49 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine} from 'react-chat-engine'; 
+import './App.css'; 
+
+const App = ()=>{
+    return(
+        <ChatEngine 
+            height="100vh"  
+            projectID="06ce8daf-7f14-4120-aedb-99dda16a0801" 
+            userName="Domo"
+            userSecret="JSreactapp"
+        />
+
+    )
+} 
+
+export default App; 
